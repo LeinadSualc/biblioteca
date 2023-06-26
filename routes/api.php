f792672e9ebc87eb628892a367ff5c21a9d906a6
Daniel Claus <danclaus2002@gmail.com>
2023-06-26 19:49:24 -0300

    ajuste endpoints

--- routes/api.php
+++ routes/api.php
@@ -16,19 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
 
-Route::get('/livros', 'LivroController@index'); //Retorna todos os livros cadastrados.
+Route::get('/livros', 'LivroController@index'); // Retorna todos os livros cadastrados.
 
-Route::get('/livros/{id}', 'LivroController@show');//retorna os detalhes do livro através do método show.
+Route::get('/livros/{id}', 'LivroController@show'); // Retorna os detalhes do livro através do método show.
 
-Route::post('/livros', 'LivroController@store');//Recebe os dados do formulário de cadastro de novos livros e grava no banco.
+Route::post('/livros', 'LivroController@store'); // Recebe os dados do formulário de cadastro de novos livros e grava no banco.
 
-Route::delete('/livros/{id}', 'LivroController@destroy');//Exclui um determinado livro.
+Route::delete('/livros/{id}', 'LivroController@destroy'); // Exclui um determinado livro.
 
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
-
-
-
-
-
-});
